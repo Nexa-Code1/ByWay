d0004b273c07c8bdd2bbd2efa7d99c015f7e2829
HagarRagab <101262480+HagarRagab@users.noreply.github.com>
2025-11-19 23:45:18 +0200

    NEX-10: sign up

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -1,5 +1,22 @@
+import ModalHeader from "./ModalHeader";
+import GoogleBtn from "./GoogleBtn";
+import TextWithLink from "../shared/TextWithLink";
+import LoginForm from "./LoginForm";
+
 function Login() {
-    return <div>Login</div>;
+    return (
+        <>
+            <ModalHeader />
+            <LoginForm />
+            <p className="text-center">or you can</p>
+            <GoogleBtn />
+            <TextWithLink
+                text="need an account?"
+                auth="signup"
+                buttonLabel="sign up"
+            />
+        </>
+    );
 }
 
 export default Login;
--- client/src/components/layout/footer/ContactUs.tsx
+++ client/src/components/layout/footer/ContactUs.tsx
@@ -5,14 +5,14 @@ function ContactUs() {
         <div>
             <h2 className="font-medium text-xl mb-2">Contact Us</h2>
             {Object.entries(contacts).map((ele) => (
-                <p className="text-sm">
+                <p className="text-sm" key={ele[0]}>
                     <span className="capitalize">{ele[0]}: </span>
                     <span>{ele[1]}</span>
                 </p>
             ))}
             <ul className="flex gap-4 items-center mt-4">
                 {links.map((link) => (
-                    <li key={link.href}>
+                    <li key={link.key}>
                         <a
                             target="_blank"
                             href={link.href}
